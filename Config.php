635ab45b27a8ca07e4abe6dbdb900d5e2de1ca94
Shawn Muktadir <shawn.muktadir03@gmail.com>
2019-03-05 18:32:14 +0600

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,8 @@
+<?php 
+ define('DB_USERNAME','root');
+ define('DB_PASSWORD','');
+ define('DB_NAME','fcm');
+ define('DB_HOST','localhost');
+
+ define('FIREBASE_API_KEY','AAAAHFYIG5M:APA91bFDGzInlJ_jbHSbZLTkXRWZLiICiIAey-EaGb2zJJ6CTmS8kFmS8CRAkXdHu8LDfjdm3mUJjbgI8KzXkl3THNdqtMVGk6_gNKUhkQQzYpavz7XYn5mRqS8E0e0Nakxu4ZcUYMR3');
+ 
